0a3b8065a7e9ab247041d7a9f7c05ffa33baa23f
Suman Bishnoi <86405366+SumanBishnoi@users.noreply.github.com>
2022-10-18 22:52:17 +0530

    Reverse Array

---
+++ reversearray.cpp
@@ -0,0 +1,16 @@
+// reverse a given array
+#include<iostream>
+using namespace std;
+int main(){
+    
+int n, arr[n];
+cout<<"enter no. of elements in array"<<"\n";
+cin>>n;
+for (int i = 0; i < n; i++)
+{
+    cin>>arr[i];
+}
+
+
+
+} 
